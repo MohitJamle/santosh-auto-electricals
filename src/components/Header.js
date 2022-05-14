d56f56dc09bc33fc126b1bcc883aa5f87738be78
Mohit jamle <bhojrajjamle@gmail.com>
2022-05-14 19:44:11 +0530

    upgrading commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
   console.log(window.innerHeight);
   return (
     <div className="header">
-      <div className="header_container"></div>
+      <div className="header_container">
+        <h1>Santosh Auto Electricals</h1>
+      </div>
     </div>
   );
 };
